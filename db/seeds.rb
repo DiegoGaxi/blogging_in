f54e283c7ec45d5e0151f27acec99cd7a4ed9037
DiegoGaxi <diego.gaxi@hotmail.com>
2024-08-16 12:44:20 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,40 @@
-if Rails.env.development?
+permissions = [
+  { name: 'admin', description: 'manage application', active: true }
+]
 
-  permissions = [
-    { name: 'admin', description: 'manage application', active: true }
-  ]
-  
-  permissions.each do |attributes|
-    Permission.find_or_create_by(attributes)
-  end
-  
-  admin_permission = Permission.find_by_name('admin')
-  
-  # create admin
-  unless User.admin.find_by_email('admin@bloggingin.com')
-    admin = User.admin.new.tap do |user|
-      user.first_name = 'admin'
-      user.paternal_surname = 'admin'
-      user.maternal_surname = 'admin'
-      user.alias = 'admin'
-      user.email = 'admin@bloggingin.com'
-      user.password = '123456'
-      user.password_confirmation = '123456'
-    end
+permissions.each do |attributes|
+  Permission.find_or_create_by(attributes)
+end
 
-    admin.transaction do
-      admin.save!(validate: false)
-      admin.user_permissions.create!(permission: admin_permission)
-    end
+admin_permission = Permission.find_by_name('admin')
+
+# create admin
+unless User.admin.find_by_email('admin@bloggingin.com')
+  admin = User.admin.new.tap do |user|
+    user.first_name = 'admin'
+    user.paternal_surname = 'admin'
+    user.maternal_surname = 'admin'
+    user.alias = 'admin'
+    user.email = 'admin@bloggingin.com'
+    user.password = '123456'
+    user.password_confirmation = '123456'
   end
-  
-  #create categories
-  categories = [
-    { name: 'Software' },
-    { name: 'Hardware' },
-    { name: 'Security' },
-    { name: 'Networking' },
-    { name: 'Other' }
-  ]
-  
-  categories.each do |attributes|
-    Category.find_or_create_by(attributes)
+
+  admin.transaction do
+    admin.save!(validate: false)
+    admin.user_permissions.create!(permission: admin_permission)
   end
+end
+
+#create categories
+categories = [
+  { name: 'Software' },
+  { name: 'Hardware' },
+  { name: 'Security' },
+  { name: 'Networking' },
+  { name: 'Other' }
+]
+
+categories.each do |attributes|
+  Category.find_or_create_by(attributes)
 end
